31cbdbbf2c4d4b19a1fbc80633a948c3e4c9980f
JFKongphop <kongphopleo@gmail.com>
2025-05-26 19:08:29 +0700

    add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,12 @@
-import hre, { ethers } from 'hardhat';
-
-const name = '';
+import { ethers } from 'hardhat';
+import bytecode from '../build/Pure.bytecode.json';
 
 const deploy = async () => {
-  const Contract = await ethers.getContractFactory(name);
+  const Contract = await ethers.getContractFactory([], bytecode);
   const contract = await Contract.deploy();
-
-  await contract.deploymentTransaction()?.wait(3);
-
   const contractAddress = await contract.getAddress();
 
-  console.log(`${name} ADDRESS:`, contractAddress);
-
-  try {
-    await hre.run('verify:verify', {
-      address: contractAddress,
-      contract: `contracts/${name}.sol:${name}`,
-      constructorArguments: [],
-    });
-  }
-  catch (e) {
-    console.log("ERROR", e);
-  }
+  console.log('ADDRESS:', contractAddress);
 }
 
 deploy().catch((err) => {
